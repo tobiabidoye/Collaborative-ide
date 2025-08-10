ea7151072ba7f2f42215e4bd8ed45ecfef312d55
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-08-10 19:45:51 -0400

    did some more with document storage support

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+        )
 html = """
 <!DOCTYPE html>
 <html>
