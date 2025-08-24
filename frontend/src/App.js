4c15185a811256da8c383a6d971662334a074e6b
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-08-24 15:53:29 -0400

    added language selection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,8 +9,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      code: '// type your code...',
-      isConnected: false
+        code: '// type your code...',
+        isConnected: false,
+        selectedLanguage: 'python'
     }
     this.websocket = null
   }
@@ -25,24 +26,6 @@ class App extends React.Component {
     }else{
         console.log("websocket data did not send");
     }
-  }
-  render() {
-    const code = this.state.code;
-    const options = {
-      selectOnLineNumbers: true
-    };
-    return (
-      <Editor
-        width="100%"
-        height="100vh"
-        language="python"
-        theme="vs-dark"
-        value={code}
-        options={options}
-        onChange={this.onChange.bind(this)}
-        editorDidMount={this.editorDidMount.bind(this)}
-      />
-    );
   }
     componentDidMount(){
         this.websocket = new WebSocket("ws://localhost:8000/ws/1");
@@ -76,6 +59,64 @@ class App extends React.Component {
             this.websocket.close();
         } 
     }
+    onLanguageChange(event){
+        const newLanguage = event.target.value; 
+        this.setState({
+            selectedLanguage: newLanguage,
+            code: this.getOrDefault(newLanguage)
+        });
+    }
+
+    getOrDefault(language){
+        const templates = {
+            'javascript': 'console.log("Hello World");',
+            'python': 'print("Hello World")',
+            'java': 'public class Main {\n    public static void main(String[] args) {\n        System.out.println("Hello World");\n    }\n}',
+            'cpp': '#include <iostream>\nusing namespace std;\n\nint main() {\n    cout << "Hello World" << endl;\n    return 0;\n}',
+            'go': 'package main\n\nimport "fmt"\n\nfunc main() {\n    fmt.Println("Hello World")\n}'
+        };
+        
+        return templates[language] || '// Hello World';
+    }
+
+
+      render() {
+        const code = this.state.code;
+        const options = {
+          selectOnLineNumbers: true
+        };
+        return (
+            <div>
+            {/* Language Selector */}
+              <div style={{padding: '10px', backgroundColor: '#1e1e1e'}}>
+                <select 
+                  value={this.state.selectedLanguage} 
+                  onChange={this.onLanguageChange.bind(this)}
+                  style={{padding: '5px', fontSize: '14px'}}
+                >
+                  <option value="javascript">JavaScript</option>
+                  <option value="python">Python</option>
+                  <option value="java">Java</option>
+                  <option value="cpp">C++</option>
+                  <option value="go">Go</option>
+                  <option value="typescript">TypeScript</option>
+                </select>
+              </div>
+
+          <Editor
+            width="100%"
+            height="100vh"
+            language={this.state.selectedLanguage}         
+            theme="vs-dark"
+            value={code}
+            options={options}
+            onChange={this.onChange.bind(this)}
+            editorDidMount={this.editorDidMount.bind(this)}
+          />
+            </div>
+        );
+      }
+ 
 }
 
 const container = document.getElementById('root');
